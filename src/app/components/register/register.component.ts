4cf1cd58b80f8cd97613b6abb8ad08739caeef8a
Quentinc63 <layk63@gmail.com>
2025-07-25 16:35:48 +0200

    feat: add register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  // Propriété représentant le formulaire
+  RegisterForm: FormGroup;
+  // Booléens d'état
+  isSubmitted = false;
+  isLoading = false;
+
+
+  constructor(private fb: FormBuilder) {
+
+    this.RegisterForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      nom: ['', [Validators.required, Validators.minLength(2)]],
+      prenom: ['', [Validators.required, Validators.minLength(2)]],
+      dateDeNaissance: ['', Validators.required]
+    });
+
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.RegisterForm.invalid) {
+      return;
+    }
+    this.isLoading = true;
+
+    // Logique de soumission ou appel API ici
+  }
 
 }
